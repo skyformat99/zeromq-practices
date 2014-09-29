90f36a61d73037e3f93cf904e38637e82f0e3a5b
uwydoc <uwydoc@gmail.com>
2014-09-29 11:35:43 +0800

    Adjust file structure, categorize files into separate folders

--- tools/build.sh
+++ tools/build.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-target="$1"
-build_dir="$2"
-[ "$target" == "" ] && target=all
-[ "$build_dir" == "" ] && build_dir=build
+build_dir=build
 
-shift && shift || true
-extra_opt="$@"
-[ ! "$extra_opt" == "" ] && extra_opt="-- $extra_opt"
-
-cmake --build "$build_dir" --use-stderr --target "$target" $extra_opt
+for target in $@; do
+    cmake --build "$build_dir" --use-stderr --target "$target" || true
+done
